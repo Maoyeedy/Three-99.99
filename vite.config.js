736607b600099761f758eabb5ba93fc04759d16c
Maoyeedy <maoyeedy@gmail.com>
2025-02-21 08:37:16 +0200

    update build with vite config

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: 'https://maoyeedy.github.io/Three-99.99/',
+  // base: './',
+  build: {
+    rollupOptions: {
+      external: [],
+    }
+  }
+})
